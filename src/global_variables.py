adafa627ede8f21c7fec61c07743e5dc7fe9bffe
Harald Thommesen <harald.thommesen@outlook.com>
2025-06-25 19:25:40 +0200

    Set up project structure and performed simple lightning exploration.

---
+++ src/global_variables.py
@@ -0,0 +1,10 @@
+RADARS = {
+    "hurum": {
+        "lat": 59.627083,
+        "lon": 10.564472,
+        "altitude": 363,
+    },
+}
+
+# Frost parameters
+FROST_ENDPOINT = "https://frost.met.no/lightning/v0.ualf"
