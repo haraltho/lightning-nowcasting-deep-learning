79ccf3dc18de00775011603219f77729e8b0f9f8
Harald Thommesen <harald.thommesen@outlook.com>
2025-09-09 13:25:21 +0200

    Reduced lead time.

--- src/build_ml_dataset.py
+++ src/build_ml_dataset.py
@@ -4,19 +4,21 @@ import local_variables as loc_vars
 import numpy as np
 import os
 import sys
+import shutil
 
 # Configurations
-run_dir = "../data/processed_data/run_3_2022_2023_2024/"
+run_dir = "../data/processed_data/run_6_leadtime15/"
 os.makedirs(run_dir, exist_ok=True)
 lightning_output_dir = run_dir + "lightning/"
 radar_output_dir     = run_dir + "radar/"
 radar_data_dir       = loc_vars.RADAR_FILE_DIR
 storms_filename = "../data/config/storm_periods.csv"
+shutil.copy(storms_filename, run_dir)
 radar = RADARS["hurum"]
 parameters = ["dBZ", "ZDR", "KDP", "RhoHV"]
-lead_time   = 30 # minutes; time to predict in the future
+lead_time   = 15 # minutes; time to predict in the future
 time_step   = 10 # minutes; length of time segments (defined by radar sweeps)
-time_window = 10 # minutes; length of time window for lightning aggregation
+time_window = 45 # minutes; length of time window for lightning aggregation
 
 print("\n== Lightning Nowcasting Pipeline ==")
 print(f"\nProcessing radar: {radar['label']}")
@@ -36,7 +38,7 @@ process_lightning(
     time_window=time_window,
     output_dir=lightning_output_dir,
 )
-
+sys.exit("asdf")
 # Step 3: Process radar data
 print("\nProcessing radar features...")
 from create_radar_features import create_radar_features
